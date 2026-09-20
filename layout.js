013b035b540dd6d1c01ac58193654cccb1fd9008
emirhanoz4141414141-web <emirhanoz4141414141@gmail.com>
2026-09-21 00:59:11 +0300

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,3 @@
+import './style.css'
+export const metadata={title:'GEYS | Gölkent Eğitim Yönetim Sistemi',description:'Gölkent Erkek Yatılı Hafızlık Kur’an Kursu eğitim yönetim sistemi'}
+export default function Layout({children}){return <html lang="tr"><body>{children}</body></html>}
