080f3cb50cf09e4fc0b7c63bdf2edb30f1026523
emirhanoz4141414141-web <emirhanoz4141414141@gmail.com>
2026-09-21 22:43:43 +0300

    brand: remove remaining GEYS metadata

--- layout.js
+++ layout.js
@@ -1,3 +1,3 @@
 import './style.css'
-export const metadata={title:'GEYS | Gölkent Eğitim Yönetim Sistemi',description:'Gölkent Erkek Yatılı Hafızlık Kur’an Kursu eğitim yönetim sistemi'}
+export const metadata={title:'E-GÖLKENT | Eğitim ve Kurum Yönetim Sistemi',description:'Gölkent Erkek Yatılı Hafızlık Kur’an Kursu dijital eğitim ve yönetim platformu'}
 export default function Layout({children}){return <html lang="tr"><body>{children}</body></html>}
